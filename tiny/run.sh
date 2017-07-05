81b639931d939bb262eb7d42a3fe5da647951879
Jaz wu <592015984b@gmail.com>
2017-07-06 03:38:11 +0800

    Add files via upload

---
+++ tiny/run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sed -i 's/127.0.0.1/'$s'/g'  /root/Applications/Shadowsocks/config.php
+sed -i 's/aes-256-cfb/'$m'/g' /root/Applications/Shadowsocks/config.php
+sed -i 's/12345678/'$k'/g' /root/Applications/Shadowsocks/config.php
+sed -i 's/443/'$p'/g' /root/Applications/Shadowsocks/config.php
+sed -i 's/1080/'$c'/g' /root/Applications/Shadowsocks/config.php
+sed -i 's/50/'$n'/g' /root/Applications/Shadowsocks/config.php
+
+php start.php start
